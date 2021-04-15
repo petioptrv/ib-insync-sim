b1ad3e2f38ec3825936bcc0eab059a1add6a6477
Petio Petrov <petioptrv@icloud.com>
2021-04-14 20:17:09 -0500

    Ran black

--- ib_sim/__init__.py
+++ ib_sim/__init__.py
@@ -1,7 +1,8 @@
 from ib_sim.ib import IB
 
-__all__ = ['IB']
+__all__ = ["IB"]
 
 from ._version import get_versions
-__version__ = get_versions()['version']
+
+__version__ = get_versions()["version"]
 del get_versions
