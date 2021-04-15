734ea8a96dca835a9e11bd50b0ceb95a9bc904a5
Petio Petrov <petioptrv@icloud.com>
2021-04-15 11:44:37 -0500

    One more try.

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ from typing import Callable
 
 from setuptools import setup, Extension
 
+from Cython.Build import cythonize
+
 try:
     from Cython.Build import cythonize
     import numpy as np
@@ -18,6 +20,9 @@ import versioneer
 
 WITH_DEBUG = False
 CYTHONIZE = isinstance(cythonize, Callable)
+
+print(f"CYTHONIZE {CYTHONIZE} - {cythonize}")
+print(f"WITH_DEBUG {WITH_DEBUG}")
 
 
 # https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
@@ -60,6 +65,9 @@ if CYTHONIZE:
     extensions = cythonize(extensions, compiler_directives=compiler_directives)
 else:
     extensions = no_cythonize(extensions)
+
+for ext in extensions:
+    print(f"{ext} - {ext.sources}")
 
 install_options = {'build_ext': {'inplace': True}}
 cmdclass = {'build_ext': build_ext} if CYTHONIZE else {}
